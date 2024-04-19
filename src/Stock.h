eb14a0ba8ef9e14fece478ed13751cfc3294d48f
d4julian <47072615+d4julian@users.noreply.github.com>
2024-04-19 01:44:13 -0400

    Modify stock class

---
+++ src/Stock.h
@@ -0,0 +1,33 @@
+#pragma once
+#include <iostream>
+#include <cmath>
+#include <sstream>
+#include "AlphaVantageAPI.h"
+
+class Stock {
+public:
+    std::string ticker;
+    double price;
+    double changeAmount;
+    double changePercentage;
+    int volume;
+    AlphaVantageAPI::function function;
+
+    Stock(std::string ticker, double price, double changeAmount, double changePercentage, double volume, AlphaVantageAPI::function function) : 
+        ticker(ticker), price(price), changeAmount(changeAmount), changePercentage(changePercentage), volume(volume), function(function) {}
+
+    Stock(std::string ticker, double price, double changeAmount, double changePercentage, double volume) : 
+        ticker(ticker), price(price), changeAmount(changeAmount), changePercentage(changePercentage), volume(volume) {}
+
+    std::string getChangePercentage() const {
+        std::stringstream stream;
+        stream << std::fixed << std::setprecision(2) << changePercentage;
+        return stream.str();
+    }
+
+    bool operator<(const Stock& other) const {
+        return function != AlphaVantageAPI::function::MOST_ACTIVELY_TRADED ? 
+        (abs(changePercentage) < abs(other.changePercentage)) : 
+        (volume < other.volume);
+    }
+};
