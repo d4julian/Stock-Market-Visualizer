66d3661c0296980afbf34629c66cb4a6e3c0ee47
d4julian <47072615+d4julian@users.noreply.github.com>
2024-04-19 01:44:47 -0400

    Update AlphaVantageAPI.h
    Refined API class

--- src/AlphaVantageAPI.h
+++ src/AlphaVantageAPI.h
@@ -2,13 +2,14 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <sstream>
 #include <curl/curl.h>
 
 class AlphaVantageAPI {
 private:
     std::string apiKey;
-    static unsigned int WriteCallback(void* contents, unsigned int size, unsigned int nmemb, std::string* userp) {
-        userp->append((char*)contents, size * nmemb);
+    static size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userp) {
+        ((std::string*)userp)->append((char*)contents, size * nmemb);
         return size * nmemb;
     }
 
@@ -17,46 +18,55 @@ public:
         apiKey = getApiKey(fileName);
     }
 
-    enum function { TIME_SERIES_INTRADAY, TIME_SERIES_DAILY, TIME_SERIES_WEEKLY, TIME_SERIES_WEEKLY_ADJUSTED, TIME_SERIES_MONTHLY, TIME_SERIES_MONTHLY_ADJUSTED, GLOBAL_QUOTE, SYMBOL_SEARCH, MARKET_STATUS, NEWS_SENTIMENT, TOP_GAINERS_LOSERS, OVERVIEW, STOCH, RSI, SIZE };
-    const std::string functionNames[SIZE] = {
-        "TIME_SERIES_INTRADAY",
-        "TIME_SERIES_DAILY",
-        "TIME_SERIES_WEEKLY",
-        "TIME_SERIES_WEEKLY_ADJUSTED",
-        "TIME_SERIES_MONTHLY",
-        "TIME_SERIES_MONTHLY_ADJUSTED",
-        "GLOBAL_QUOTE",
-        "SYMBOL_SEARCH",
-        "MARKET_STATUS",
-        "NEWS_SENTIMENT",
-        "TOP_GAINERS_LOSERS",
-        "OVERVIEW",
-        "STOCH",
-        "RSI"
-    };
+    enum function { TOP_GAINERS_LOSERS, TOP_GAINERS, TOP_LOSERS, MOST_ACTIVELY_TRADED };
+    static std::string getFunctionName(const int& index) {
+        switch (index) {
+            default:
+            case 0:
+                return "TOP_GAINERS_LOSERS";
+            case 1:
+                return "top_gainers";
+            case 2:
+                return "top_losers";
+            case 3:
+                return "most_actively_traded";
+        }
+    }
 
-    std::string fetchData(const function& f) {
+    std::string fetchData() {
         CURL* curl;
         CURLcode res;
         std::string readBuffer;
-        //std::string url = "https://www.alphavantage.co/query?function=" + functionNames[f] + "&symbol=" + symbol + "&interval=" + interval + "&apikey=" + apiKey;
-        std::string url = "https://www.alphavantage.co/query?function=" + functionNames[f] + "&apikey=" + apiKey;
+        std::string url = "https://www.alphavantage.co/query?function=" + getFunctionName(TOP_GAINERS_LOSERS) + "&apikey=" + apiKey;
         std::cout << "URL: " << url << std::endl;
 
         curl = curl_easy_init();
-        if(curl) {
-            curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+        if (curl) {
+            curl_easy_setopt(curl, CURLOPT_URL, url);
             curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
             curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
             res = curl_easy_perform(curl);
             curl_easy_cleanup(curl);
 
             if(res != CURLE_OK) {
-                std::cerr << "curl_easy_perform() failed: " << curl_easy_strerror(res) << std::endl;
-                return "";
+                std::cerr << "Could not fetch from URL: " << curl_easy_strerror(res) << std::endl;
+                exit (0);
             }
         }
         return readBuffer;
+    }
+    
+    std::string readFile(const std::string& filename) {
+        std::ifstream file(filename);
+        if (!file.is_open()) {
+            std::cerr << "Failed to open file: " << filename << std::endl;
+            exit (0);
+        }
+
+        std::stringstream buffer;
+        buffer << file.rdbuf();
+        file.close();
+        return buffer.str();
     }
 
     std::string getApiKey(const std::string& filename) {
