7f5634429d3d27d999a53bf5238ef814ba406b03
d4julian <47072615+d4julian@users.noreply.github.com>
2024-04-19 02:08:05 -0400

    Updated name of class, refined operation of heap and map

---
+++ src/StockProcessor.h
@@ -0,0 +1,41 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <queue>
+#include "Stock.h"
+#include "AlphaVantageAPI.h"
+
+using namespace std;
+
+class StockMarketVisualizer {
+private:
+    AlphaVantageAPI::function function;
+public:
+    StockMarketVisualizer(AlphaVantageAPI::function function) : function(function) {}
+
+    std::priority_queue<Stock> maxHeap;
+    std::unordered_map<std::string, Stock> stockMap;
+
+    void addStock(const std::string& ticker, double price, double change, double changePercentage, double volume) {
+        Stock stock(ticker, price, change, changePercentage, volume, function);
+        stockMap.emplace(ticker, stock);
+        maxHeap.push(stock);
+    }
+
+    vector<Stock> getHeap() {
+        vector<Stock> result;
+        auto tempHeap = maxHeap;
+        while (!tempHeap.empty()) {
+            Stock topStock = tempHeap.top();
+            tempHeap.pop();
+            result.push_back(topStock);
+        }
+        return result;
+    }
+
+    void clearHeap() {
+        while (!maxHeap.empty()) maxHeap.pop();
+        stockMap.clear();
+    }
+};
